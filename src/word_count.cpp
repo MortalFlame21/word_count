96fc41322b60c661dde550b5a586fb95a8a54a36
MortalFlame21 <markgio2100@gmail.com>
2026-03-22 20:47:48 +1100

    word_count.cpp. impl basic code output.

--- src/word_count.cpp
+++ src/word_count.cpp
@@ -1,102 +1,155 @@
 #include <bitset>
+#include <cctype>
+#include <deque>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <vector>
 
-enum opts { ALL, WORDS, CHARACTERS, LINES, BYTES, VERBOSE, STD_IN, MAX_OPTS };
-std::bitset<MAX_OPTS> g_opts{};
-std::fstream g_fs{};
+// classes & enums
+struct count_t {
+    int words{};
+    int characters{};
+    int lines{};
+    int bytes{};
+};
 
-void set_opt(char opt) {
-    switch (opt) {
-    case 'a':
-        g_opts.set(opts::ALL);
-        return;
-    case 'w':
-        g_opts.set(opts::WORDS);
-        return;
-    case 'c':
-        g_opts.set(opts::CHARACTERS);
-        return;
-    case 'l':
-        g_opts.set(opts::LINES);
-        return;
-    case 'b':
-        g_opts.set(opts::BYTES);
-        return;
-    case 'v':
-        g_opts.set(opts::VERBOSE);
-        return;
-    case 'i':
-        g_opts.set(opts::STD_IN);
-        return;
-    default:
-        return;
-    }
-}
+struct info_t {
+    std::string label{};
+    count_t count{};
+};
+
+enum opts { WORDS, CHARACTERS, LINES, BYTES, VERBOSE, STD_IN, MAX_OPTS };
+
+// constants
+const std::bitset<opts::MAX_OPTS> OPT_ALL{(1 << opts::WORDS) | (1 << opts::CHARACTERS) |
+                                          (1 << opts::LINES)};
+
+// globals
+std::bitset<opts::MAX_OPTS> g_opts{};
+std::deque<std::filesystem::path> g_paths{};
+std::vector<info_t> g_counts{};
 
 void set_opt(std::string_view opt) {
-    std::cout << opt << "\n";
-    if (opt == "all")
-        g_opts.set(opts::ALL);
-    else if (opt == "words")
+    if (opt == "all" || opt == "a")
+        g_opts |= OPT_ALL;
+    else if (opt == "words" || opt == "w")
         g_opts.set(opts::WORDS);
-    else if (opt == "chars")
+    else if (opt == "chars" || opt == "c")
         g_opts.set(opts::CHARACTERS);
-    else if (opt == "lines")
+    else if (opt == "lines" || opt == "l")
         g_opts.set(opts::LINES);
-    else if (opt == "bytes")
+    else if (opt == "bytes" || opt == "b")
         g_opts.set(opts::BYTES);
-    else if (opt == "verbose")
+    else if (opt == "verbose" || opt == "v")
         g_opts.set(opts::VERBOSE);
-    else if (opt == "std-in")
+    else if (opt == "std-in" || opt == "i")
         g_opts.set(opts::STD_IN);
-}
-
-void set_opt_char(std::string_view opts) {
-    for (auto& c : opts)
-        set_opt(c);
 }
 
 void parse_opt(std::string_view str) {
-    std::cout << str << '\n';
     if (str.size() == 1)
         return; // nothing
 
-    if (str.substr(0, 2) == "--")
+    // parse whole word
+    if (str.starts_with("--"))
         set_opt(str.substr(2, str.size() - 2));
-    else if (str[0] == '-')
-        set_opt_char(str.substr(1, str.size() - 1));
+    // parse individual characters of word
+    else if (str.starts_with('-'))
+        for (int i{}; i < str.size(); ++i)
+            set_opt(str.substr(i, 1));
 }
 
-void parse_flags(int argc, char *const argv[]) {
-    for (int i{1}; i < argc - 1; ++i)
+void parse_flags(int argc, char* const argv[]) {
+    for (int i{1}; i < argc; ++i) {
+        std::string_view arg{argv[i]};
+        // only consider flags before "--"
+        if (arg == "--")
+            break;
         parse_opt(argv[i]);
+    }
 
     if (g_opts.none())
-        g_opts.set(opts::ALL);
+        g_opts |= OPT_ALL;
 }
 
-void parse_file(int argc, char *const argv[]) {
-    std::string f{argv[argc - 1]};
-    g_fs.open(f);
+void parse_files(int argc, char* const argv[]) {
+    namespace fs = std::filesystem;
 
-    if (std::filesystem::is_directory(f))
-        throw std::runtime_error(f + " is a directory!\n");
-    else if (!std::filesystem::exists(f))
-        throw std::runtime_error(f + " does not exist!\n");
-    else if (!g_fs)
-        throw std::runtime_error(f + " failed to open!\n");
-    else // assume it is not a regular file
-        throw std::runtime_error(f + " needs to be a regular file!\n");
+    bool parse_as_files{};
+    for (int i{1}; i < argc; ++i) {
+        std::string arg{argv[i]};
+
+        // parse next args as actual file name
+        if (arg == "--" && !parse_as_files)
+            parse_as_files = true;
+        // after "--" parse all args as files
+        // OR not seen "--" or before it, skip flags, flags begin with '-'.
+        else if (parse_as_files || !arg.starts_with('-'))
+            g_paths.push_back(arg);
+    }
+
+    if (g_paths.empty())
+        g_opts.set(opts::STD_IN);
 }
 
-void count() {}
+count_t count(std::istream& in) {
+    count_t ct{};
+    // either read bytes from ifs or make user input via std::cin
+    std::stringstream ss{};
+    ss << in.rdbuf();
+    auto str{ss.str()};
 
-int main(int argc, char *argv[]) {
+    for (bool in_word{}; const auto& c : str) {
+        if (std::isalnum(c)) {
+            ++ct.characters;
+            in_word = true;
+        } else if (std::isspace(c)) {
+            ++ct.characters;
+            if (in_word)
+                ++ct.words;
+            if (c == '\n')
+                ct.lines;
+            in_word = false;
+        }
+    }
+    ct.bytes = str.size();
+
+    return ct;
+}
+
+void print(const info_t& info) {
+    if (g_opts.test(opts::VERBOSE)) {
+        std::cout << info.label << '\n';
+        std::cout << "\twords: " << info.count.words << '\n';
+        std::cout << "\tcharacters: " << info.count.characters << '\n';
+        std::cout << "\tnewlines: " << info.count.lines << '\n';
+        std::cout << "\tbytes: " << info.count.bytes << '\n';
+    } else {
+        std::cout << info.label << " | " << info.count.words << ", "
+                  << info.count.characters << ", " << info.count.lines << ", "
+                  << info.count.bytes << '\n';
+    }
+}
+
+int main(int argc, char* argv[]) {
     parse_flags(argc, argv);
-    parse_file(argc, argv);
+    std::cout << g_opts << '\n';
+    parse_files(argc, argv);
+
+    namespace fs = std::filesystem;
+
+    for (auto& path : g_paths) {
+        if (std::fstream ifs{path}; !fs::is_regular_file(path) || !fs::is_symlink(path))
+            print({path.string(), count(ifs)});
+        else
+            std::cout << "WARNING: " << path
+                      << " is not a valid file path or bad file.\n";
+    }
+
+    if (g_opts.test(opts::STD_IN))
+        print({"std-in", count(std::cin)});
 }
